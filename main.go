98cb10b01145d01b5380a51f35ada2da21246f89
James Lucktaylor <jlucktay+github@gmail.com>
2019-03-24 14:53:46 +0000

    Put it all together

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/sheets/v4"
 )
 
 func main() {
+	b, err := ioutil.ReadFile("credentials.json")
+	if err != nil {
+		log.Fatalf("Unable to read client secret file: %v", err)
+	}
+
+	// If modifying these scopes, delete your previously saved token.json.
+	config, err := google.ConfigFromJSON(b, "https://www.googleapis.com/auth/spreadsheets.readonly")
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: %v", err)
+	}
+	client := getClient(config)
+
+	srv, err := sheets.New(client)
+	if err != nil {
+		log.Fatalf("Unable to retrieve Sheets client: %v", err)
+	}
+
+	for _, sub := range subtractPatternsFromFails(getPatterns(srv), getLintFails()) {
+		fmt.Println(sub)
+	}
 }
